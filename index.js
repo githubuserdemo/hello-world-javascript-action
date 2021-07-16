8827f7c68290699ee33551223b9e4a4f4067c2ab
Wechuli <wechulipaul@yahoo.com>
2021-07-16 15:33:33 +0300

    add custom modules

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import core from "@actions/core";
 import github from "@actions/github";
+import { add, sub } from "./custommodule";
 
 try {
   // `who-to-greet` input defined in action metadata file
@@ -10,6 +11,10 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2);
   console.log(`The event payload: ${payload}`);
+
+  //   custom stuff
+  console.log(add(1, 2));
+  console.log(sub(3, 2));
 } catch (error) {
   core.setFailed(error.message);
 }
